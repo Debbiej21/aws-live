9a17ff19ea56ae0213d3bbb371e852dae43c9339
Debbiej21 <100556173+Debbiej21@users.noreply.github.com>
2023-04-11 15:11:29 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cvt8qbqpbpdh.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "deborahjoseph-employee"
+customregion = "us-east-1"
